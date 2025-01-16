50e4d6fa0084bdcab1a6d934b40f26e0e7598069
Deepak Chhetri <deepak.curator@gmail.com>
2025-01-16 22:40:28 +0200

    test

--- course-settings.js
+++ course-settings.js
@@ -6,16 +6,12 @@ const courseSettings = {
   subtitle: "Python",
   slug: "laurea-python-english-2025",
   organizationName: "laurea",
-  tmcCourse: "programming-25",
-  quizzesId: "e0625f9b-626a-4941-afda-6c6e1b577045",
+  tmcCourse: "laurea-python-english-2025",
+  quizzesId: "0a8162e8-133a-4fa8-b619-4125cb92c6e0",
   tmcOrganization: "laurea",
   bannerPath: "banner.svg",
   showExerciseDescriptionWhenNotLoggedIn: true,
   courseVariants: [
-    {
-      tmcOrganization: "hbc",
-      tmcCourse: "python-TVT24Eng"
-    },
     {
       tmcOrganization: "insviladegracia",
       tmcCourse: "python-viladegracia-2024"
@@ -23,7 +19,7 @@ const courseSettings = {
     {
       tmcOrganization: "laurea",
       tmcCourse: "laurea-python-english-2025"
-    }
+    },
     // {
     //   tmcOrganization: "hbc",
     //   tmcCourse: "python-TVT21A",
