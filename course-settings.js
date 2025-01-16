e59d1b32b79ff6dac42fef57be8de3837a161860
Deepak Chhetri <deepak.curator@gmail.com>
2025-01-16 23:32:07 +0200

    test

--- course-settings.js
+++ course-settings.js
@@ -1,29 +1,25 @@
 const courseSettings = {
-  name: "Fundamentals of Programming",
-  siteUrl: "https://dipaish.github.io/programming-24",
-  githubUrl: "https://github.com/dipaish/programming-24",
-  subtitle: "Python",
-  slug: "programming-24",
-  organizationName: "MOOC",
+  language: "en",
+  name: "Python Programming MOOC 2025",
+  siteUrl: "https://programming-25.mooc.fi",
+  githubUrl: "https://github.com/rage/programming-25",
+  subtitle: "Python Programming MOOC",
+  slug: "laurea-python-english-2025",
+  organizationName: "laurea",
   tmcCourse: "programming-25",
   quizzesId: "e0625f9b-626a-4941-afda-6c6e1b577045",
   tmcOrganization: "mooc",
   bannerPath: "banner.svg",
   showExerciseDescriptionWhenNotLoggedIn: true,
   courseVariants: [
-    {
-      tmcOrganization: "laurea",
-      tmcCourse: "laurea-python-english-2025"
-    },
-    {
-      tmcOrganization: "hbc",
-      tmcCourse: "python-TVT24Eng"
-    },
-    {
-      tmcOrganization: "insviladegracia",
-      tmcCourse: "python-viladegracia-2024"
-    },
-
+    // {
+    //   tmcOrganization: "hbc",
+    //   tmcCourse: "python-TVT24Eng"
+    // },
+    // {
+    //   tmcOrganization: "insviladegracia",
+    //   tmcCourse: "python-viladegracia-2024"
+    // },
     // {
     //   tmcOrganization: "hbc",
     //   tmcCourse: "python-TVT21A",
@@ -31,4 +27,5 @@ const courseSettings = {
     // },
   ],
 }
+
 module.exports = courseSettings
