d1e90abbb684e6dd62d987f933f2239bd25548ec
Deepak Chhetri <deepak.curator@gmail.com>
2025-01-31 12:08:08 +0200

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,79 +1,19 @@
 import React, { useContext } from "react"
-import styled from "styled-components"
-import BackgroundImage from "../images/banner.svg"
-import { Card, CardContent } from "@material-ui/core"
 import { withTranslation } from "react-i18next"
-import withSimpleErrorBoundary from "../util/withSimpleErrorBoundary"
-import CourseSettings from "../../course-settings"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
-import PagesContext from "../contexes/PagesContext"
-
-const StyledIcon = styled(FontAwesomeIcon)`
-  color: black;
-  margin-bottom: 1rem;
-  margin: 1rem;
-`
-
-const SocialContainer = styled.div``
-
-const ContentContainer = styled.div`
-  padding: 1rem 0;
-`
-
-const GithubContainer = styled.div`
-  padding-top: 1rem;
-  a {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-`
-
-const ButtonContainer = styled.div`
-  padding: 1rem 0;
-`
-
-const FooterWrapper = styled.footer`
-  height: 35rem;
-  position: relative;
-  a {
-    color: #006fe6;
-  }
-`
-
-const FooterBackground = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  width: 100%;
-  height: 100%;
-  background-image: url(${BackgroundImage});
-  filter: invert(1) grayscale(1) brightness(1.5) opacity(0.5);
-  z-index: -50000;
-`
-
-const FooterContent = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-`
-
-const StyledCard = styled(Card)`
-  width: 90%;
-  max-width: 800px;
-`
-
-const StyledCardContent = styled(CardContent)`
-  flex-direction: column;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import PagesContext from "../context/PagesContext"
+import CourseSettings from "../../course-settings"
+import withSimpleErrorBoundary from "../util/withSimpleErrorBoundary"
+import {
+  FooterWrapper,
+  FooterBackground,
+  FooterContent,
+  StyledCard,
+  StyledCardContent,
+  GithubContainer,
+  StyledIcon,
+} from "./FooterStyles"
 
 const Footer = ({ t }) => {
   const pagesContextValue = useContext(PagesContext)
@@ -85,7 +25,7 @@ const Footer = ({ t }) => {
       <FooterContent>
         <StyledCard>
           <StyledCardContent>
-            <GithubContainer>
+            <div>
               <a
                 href={CourseSettings.githubUrl}
                 target="_blank"
@@ -94,31 +34,19 @@ const Footer = ({ t }) => {
                 <StyledIcon icon={faGithub} size="3x" title={t("footer-src")} />
                 <div>{t("footer-src")}</div>
               </a>
-            </GithubContainer>
-          </StyledCardContent>
-        </StyledCard>
-        <StyledCard>
-          <StyledCardContent>
-            <ContentContainer>
-              <p>© {new Date().getFullYear()} {CourseSettings.name}</p>
-              <p>
+            </div>
+            <div>
                 <a
                   href="https://github.com/dipaish/programming-24/blob/main/license.md"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Apache License, Version 2.0 & Creative Commons BY-NC-SA 4.0
+                  © {new Date().getFullYear()} Apache License, Version 2.0 & Creative Commons BY-NC-SA 4.0
                 </a>
-              </p>
-            </ContentContainer>
-          </StyledCardContent>
-        </StyledCard>
-        <StyledCard>
-          <StyledCardContent>
-            <ContentContainer>
-              <p>This content has been tailored for the Fundamentals of Programming course at Laurea University of Applied Sciences by </p>
-              <p>: <a href="https://github.com/dipaish" target="_blank" rel="noopener noreferrer">Deepak Chhetri</a> and <a href="https://github.com/kmhentto" target="_blank" rel="noopener noreferrer">Katja Henttonen</a></p>
-            </ContentContainer>
+            </div>
+            <div>
+              <p>his content has been tailored for the Fundamentals of Programming course at Laurea University of Applied Sciences by:<a href="https://github.com/dipaish" target="_blank" rel="noopener noreferrer">Deepak Chhetri</a> and <a href="https://github.com/kmhentto" target="_blank" rel="noopener noreferrer">Katja Henttonen</a></p>
+            </div>
           </StyledCardContent>
         </StyledCard>
       </FooterContent>
