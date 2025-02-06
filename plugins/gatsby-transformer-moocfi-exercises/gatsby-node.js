8b5bd721ca7539c6756865190861ae837fcf757a
Katja Henttonen <39044068+kmhentto@users.noreply.github.com>
2025-02-06 15:32:35 +0200

    Update gatsby-node.js

--- plugins/gatsby-transformer-moocfi-exercises/gatsby-node.js
+++ plugins/gatsby-transformer-moocfi-exercises/gatsby-node.js
@@ -1,6 +1,5 @@
-const GraphQLString = require("gatsby/graphql").GraphQLString
-const GraphQLList = require("gatsby/graphql").GraphQLList
-const GraphQLObjectType = require("gatsby/graphql").GraphQLObjectType
+const { fetchProgrammingExerciseDetails } = require("../../services/moocfi") // Import API call
+const { GraphQLList, GraphQLObjectType, GraphQLString } = require("gatsby/graphql")
 
 const quizRegex = /<\s*quiz\s*id\s*=\s*['"]\s*([\w-]+)\s*['"]\s*>/gm
 const crowdsorcererRegex = /<\s*crowdsorcerer\s*id\s*=\s*['"]\s*(\w+)\s*['"].*>/gm
@@ -11,20 +10,14 @@ const moodleRegex = /<\s*moodle-exercise\s*name\s*=\s*['"]\s*(.*)\s*['"]\s*>/gm
 const sqlTrainerRegex = /<\s*sqltrainer-exercise\s*name\s*=\s*['"]\s*(.*)\s*['"]\s*>/gm
 const commentRegex = /<!--.*?-->/mgs
 
-
-function getMatches(string, regex, index) {
-  index || (index = 1) // default to the first capturing group
-  var matches = []
-  var match
+function getMatches(string, regex, index = 1) {
+  let matches = []
+  let match
   while ((match = regex.exec(string))) {
-    const location = match.index
-    matches.push({ match: match[index], location })
+    matches.push({ match: match[index], location: match.index })
   }
   return matches
 }
-
-const { fetchProgrammingExerciseDetails } = require("../../services/moocfi") // Import API call
-const { GraphQLList, GraphQLObjectType, GraphQLString } = require("gatsby/graphql")
 
 const ExerciseType = new GraphQLObjectType({
   name: `Exercise`,
@@ -49,12 +42,12 @@ exports.setFieldsOnGraphQLNodeType = ({ type }) => {
               let id = "unknown"
               try {
                 const match = getMatches(res.match, programmingExerciseNameRegex, 1)[0].match
-                id = match.substring(1, match.length - 1) // Fix: Corrected substring extraction
+                id = match.substring(1, match.length - 1) // Fix substring extraction
               } catch (e) {
                 console.error(`Error extracting exercise name: ${e}`)
               }
 
-              // Fetch Exercise Details
+              //  Fetch Exercise Details
               let exerciseDetails = {}
               try {
                 exerciseDetails = await fetchProgrammingExerciseDetails(id)
@@ -83,96 +76,10 @@ exports.setFieldsOnGraphQLNodeType = ({ type }) => {
       },
     }
   }
+
   return {}
 }
-          const programmingExercises = getMatches(
-            source,
-            programmingExerciseTagRegex,
-            1,
-          ).map(res => {
-            let id = "unknown"
-            try {
-              const match = getMatches(
-                res.match,
-                programmingExerciseNameRegex,
-                1,
-              )[0].match
-              id = match.substr(1, match.length - 2)
-            } catch (e) {}
-            return {
-              id,
-              location: res.location,
-              type: "programming-exercise",
-              parentPagePath: node.frontmatter.path,
-            }
-          })
 
-          const inBrowserProgrammingExercises = getMatches(
-            source,
-            inBrowserProgrammingExerciseTagRegex,
-            1,
-          ).map(res => {
-            let id = "unknown"
-            try {
-              const match = getMatches(
-                res.match,
-                programmingExerciseNameRegex,
-                1,
-              )[0].match
-              id = match.substr(1, match.length - 2)
-            } catch (e) {}
-            return {
-              id,
-              location: res.location,
-              type: "programming-exercise",
-              parentPagePath: node.frontmatter.path,
-            }
-          })
-
-          const crowdsorcerers = getMatches(source, crowdsorcererRegex, 1).map(
-            res => {
-              return {
-                id: res.match,
-                location: res.location,
-                type: "crowdsorcerer",
-                parentPagePath: node.frontmatter.path,
-              }
-            },
-          )
-
-          const moodles = getMatches(source, moodleRegex, 1).map(res => {
-            return {
-              id: res.match,
-              location: res.location,
-              type: "moodle-exercise",
-              parentPagePath: node.frontmatter.path,
-            }
-          })
-
-          const sqlTrainers = getMatches(source, sqlTrainerRegex, 1).map(
-            res => {
-              return {
-                id: res.match,
-                location: res.location,
-                type: "sqltrainer-exercise",
-                parentPagePath: node.frontmatter.path,
-              }
-            },
-          )
-
-          return programmingExercises
-            .concat(quizzes)
-            .concat(crowdsorcerers)
-            .concat(moodles)
-            .concat(sqlTrainers)
-            .concat(inBrowserProgrammingExercises)
-            .sort(function(a, b) {
-              return a.location - b.location
-            })
-        },
-      },
-    }
-  }
 
   // by default return empty object
   return {}
